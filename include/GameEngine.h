3ec7e53a617ed60ecd6a12890472344cb667e293
Hoang Tran Duc Hai <hoangtranduchai@gmail.com>
2025-12-13 13:50:00 +0700

    Update Singleton Pattern

--- include/GameEngine.h
+++ include/GameEngine.h
@@ -10,32 +10,11 @@
 class IGameState;
 
 class GameEngine {
-private:
-    // Lưu trữ các trạng thái game
-    std::vector<IGameState*> m_States;
-
-    bool m_bRunning;
-    SDL_Window* m_pWindow;
-    SDL_Renderer* m_pRenderer;
-    SDL_Texture* m_pPlayerTexture; // "Kết cấu" của nhân vật
-    SDL_Rect m_playerDestRect; // "Trạng thái" của nhân vật (Vị trí và Kích thước)
-
-    // "Tốc độ" của nhân vật (Pixel/giây)
-    float m_playerSpeed; 
-
-    // "Trạng thái" Input của người chơi (Di chuyển)
-    bool m_movingUp;
-    bool m_movingDown;
-    bool m_movingLeft;
-    bool m_movingRight;
-
-    // Kích thước cửa sổ để giới hạn di chuyển
-    int m_windowWidth;
-    int m_windowHeight;
-
 public:
-    GameEngine();
-    ~GameEngine();
+    // Mẫu thiết kế Singleton: Trả về thể hiện duy nhất
+    static GameEngine* GetInstance();
+    // Hàm hủy Instance
+    static void DestroyInstance();
 
     // Cập nhật hàm Init để nhận thông tin cửa sổ
     bool Init(const char* title, int x, int y, int w, int h, bool fullscreen);
@@ -51,4 +30,38 @@ public:
     bool IsRunning() {
         return m_bRunning;
     }
+
+    // Xóa bỏ Copy Constructor và Assignment Operator để đảm bảo tính ĐỘC NHẤT
+    GameEngine(const GameEngine&) = delete;
+    void operator=(const GameEngine&) = delete;
+
+private:
+    // Private constructor cho Singleton
+    GameEngine();
+    ~GameEngine();
+
+    // Biến tĩnh lưu trữ thể hiện duy nhất
+    static GameEngine* s_Instance;
+
+    // Lưu trữ các trạng thái game
+    std::vector<IGameState*> m_States;
+
+    bool m_bRunning;
+    SDL_Window* m_pWindow;
+    SDL_Renderer* m_pRenderer;
+
+    SDL_Texture* m_pPlayerTexture; // "Kết cấu" của nhân vật
+    SDL_Rect m_playerDestRect; // "Trạng thái" của nhân vật (Vị trí và Kích thước)
+    SDL_Texture* m_pBackgroundTexture; // "Kết cấu" của nền game
+    float m_playerSpeed; // "Tốc độ" của nhân vật (Pixel/giây)
+
+    // "Trạng thái" Input của người chơi (Di chuyển)
+    bool m_movingUp;
+    bool m_movingDown;
+    bool m_movingLeft;
+    bool m_movingRight;
+
+    // Kích thước cửa sổ để giới hạn di chuyển
+    int m_windowWidth;
+    int m_windowHeight;
 };
